7451e81840501e41f63c5d8468b78c7e54f31dae
tong <425663303@qq.com>
2020-01-09 23:48:07 +0800

    Draft: class construction

---
+++ NewsPublish.Service/Db.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NewsPublish.Model.Entity;
+
+namespace NewsPublish.Service
+{
+    /// <summary>
+    /// 数据库访问上下文
+    /// </summary>
+    class Db : DbContext
+    {
+        public Db() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder); //base: mother class=DbContext
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-33O7GCQ2; Initial Catalogue=NewsPublish; User Id=tongtong; Password:123456",
+                b => b.UseRowNumberForPaging());
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected virtual DbSet<Banner> Banner { get; set; }
+        protected virtual DbSet<NewsClassify> NewsClassify { get; set; }
+        protected virtual DbSet<News> News { get; set; }
+        protected virtual DbSet<NewsComment> NewsComment { get; set; }
+    }
+}
